13c00974ddc2a30dec45c6498ce38f0bc5a747de
Diego Martinez Giardini <diegomartinezgiardini@Air-de-Diego.home>
2021-05-03 18:13:26 -0300

    Mi primer proyecto de GIT :wq :wq
    :wq

---
+++ js/main.js
@@ -0,0 +1,60 @@
+	// Array con las imagenes que se iran mostrando en la web
+var imagenes=new Array(
+    'img/covid1.jpeg',
+    'img/covid2.jpeg',
+);
+
+var x=0;
+setInterval(function () {
+
+    // cambiamos la imagen de manera sucesiva
+
+    document.getElementById("covid1").src=imagenes[x++%imagenes.length];
+
+},4000);
+
+$(".menucel").click(function(){ 
+    event.stopPropagation()
+$(".mobilemenu").animate({'top':'0px',},500);
+});
+$("body,btn").click(function(){
+$(".mobilemenu").animate({'top':'-300px',},500);
+});
+
+//Envio de formulario a email:
+
+document.addEventListener('DOMContentLoaded', () => {
+    function formSendInfo() {
+        document.addEventListener('submit', (e) => {    
+        }) 
+    }
+    formSendInfo();
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    function textAreaValidate() {
+      const $textArea = document.getElementById("message");
+      const $span = document.createElement("span");
+      $span.id = $textArea.name;
+      $span.textContent = $textArea.title;
+      $span.classList.add("textarea-error", "none");
+      $textArea.insertAdjacentElement("afterend", $span)
+    }
+    document.addEventListener("keyup", (e) => {
+      if (e.target.matches(".contact-form [textarea]")) {
+        let $input = e.target;
+        let pattern = $input.dataset.pattern;
+  
+        if (pattern && $input.value !== "") {
+          let regex = new RegExp(pattern);
+          return !regex.exec($input.value) ? document.getElementById($input.name).classList.add("is-active") : document.getElementById($input.name).classList.remove("is-active");
+        }
+  
+        if (!pattern) {
+          return $input.value === "" ? document.getElementById($input.name).classList.add("is-active") : document.getElementById($input.name).classList.remove("is-active");
+        }
+      }
+    });
+    textAreaValidate();
+  });
